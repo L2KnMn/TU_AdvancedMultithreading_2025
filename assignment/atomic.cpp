c1d31517246c783560ed41e5258460d71abc1db5
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-04-07 13:08:32 +0900

    세밀한 동기화를 구현한 과제04 구현

--- assignment/atomic.cpp
+++ assignment/atomic.cpp
@@ -3,7 +3,18 @@
 #include <atomic>
 #include <chrono>
 
+struct Memory {
+	int value;
+};
 
+bool CAS(Memory memory, Memory expected, Memory update) {
+	if (memory.value == expected.value) {
+		memory.value = update.value;
+		return true;
+	}
+	else
+		return false;
+}
 
 int main() {
 
